4d67b06b575b3a70be7b171f90c8c1cdff324201
Nathen Wiley <nzw0037@auburn.edu>
2024-02-17 00:36:22 -0800

    Completed Converter

--- CurrencyConverter/ConversionResultsViewController.swift
+++ CurrencyConverter/ConversionResultsViewController.swift
@@ -8,20 +8,16 @@
 import UIKit
 
 class ConversionResultsViewController: UIViewController {
-    // Outlets for the labels to display the converted amounts
+    
     @IBOutlet weak var labelPeso: UILabel!
     @IBOutlet weak var labelEuro: UILabel!
     @IBOutlet weak var labelPound: UILabel!
     @IBOutlet weak var labelYen: UILabel!
     
-    // Variables to hold the formatted converted currency values
     var convertedAmounts: [String: String]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Update each label with the corresponding converted amount
-        // If a currency was not selected, it will not be displayed
         labelPeso.text = convertedAmounts?["peso"] ?? "Not Selected"
         labelEuro.text = convertedAmounts?["euro"] ?? "Not Selected"
         labelPound.text = convertedAmounts?["pound"] ?? "Not Selected"
--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -12,16 +12,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var textFieldUSD: UITextField!
     @IBOutlet weak var errorLabel: UILabel!
     
-    // UISwitch outlets
     @IBOutlet weak var switchPeso: UISwitch!
     @IBOutlet weak var switchEuro: UISwitch!
     @IBOutlet weak var switchPound: UISwitch!
     @IBOutlet weak var switchYen: UISwitch!
     
-    // CurrencyConverter instance
     let currencyConverter = CurrencyConverter()
     
-    // Variables to hold converted currency values
     var convertedPeso: String?
     var convertedEuro: String?
     var convertedPound: String?
@@ -30,36 +27,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         textFieldUSD.keyboardType = .numberPad
-        errorLabel.text = "" // Hide error label initially
+        errorLabel.text = ""
     }
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
-        // Validate input
         guard let text = textFieldUSD.text, let dollars = Int(text) else {
             errorLabel.text = "Invalid input. Please enter a valid amount."
             return
         }
         
-        // Reset error label
         errorLabel.text = ""
         
-        // Perform conversion
         let convertedValues = currencyConverter.convert(dollars: dollars)
         
-        // Set the converted amounts if the respective switch is on
         convertedPeso = switchPeso.isOn ? currencyConverter.format(currencyValue: convertedValues.pesos) : nil
         convertedEuro = switchEuro.isOn ? currencyConverter.format(currencyValue: convertedValues.euros) : nil
         convertedPound = switchPound.isOn ? currencyConverter.format(currencyValue: convertedValues.pounds) : nil
         convertedYen = switchYen.isOn ? currencyConverter.format(currencyValue: convertedValues.yen) : nil
         
-        // Perform segue to show results
         performSegue(withIdentifier: "showConversionSegue", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showConversionSegue" {
             if let destinationVC = segue.destination as? ConversionResultsViewController {
-                // Pass the converted and formatted amounts to the destination view controller
                 destinationVC.convertedAmounts = [
                     "peso": convertedPeso,
                     "euro": convertedEuro,
